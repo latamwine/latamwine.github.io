fd32eb241200db23b0333abb99b2cebfb9992d09
Jan Dziedziak <jas.dzied@gmail.com>
2025-06-25 15:39:59 +0100

    improved formatting

--- src/App.tsx
+++ src/App.tsx
@@ -13,15 +13,56 @@ type ProductProps = {
 
 function ProductView({ data }: ProductProps) {
   const imageUrl = data.images[0].src
+  let attributes = new Map<string, string>()
+  data.attributes.forEach(x => {
+    attributes.set(x.slug, x.options[0])
+  })
 
   return <div style={{ width: "100vw", height: "200vh" }}>
-    <div style={{ width: "100vw", height: "100vh", backgroundColor: "red" }}>
-      <div>
-        <img src={imageUrl} crossOrigin="anonymous" />
+    <div style={{ width: "100vw", height: "100vh", backgroundImage: 'url("background.png")' }}>
+      <div style={{
+        backgroundColor: 'white', width: "calc(100vw - 6vw)", height: "calc(100vh - 6vw)",
+        position: "relative",
+        top: "3vw",
+        left: "3vw",
+        borderRadius: "5px"
+      }}>
+        <div style={{
+          width: "92%", height: "40%", display: "flex", flexDirection: "row", justifyContent: "flex-start", alignItems: "stretch", padding: "4%",
+        }}>
+          <img src={imageUrl} crossOrigin="anonymous" style={{
+            objectFit: "cover",
+            width: "30%",
+            height: "100%",
+            paddingRight: "4%",
+          }} />
+          <div style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+          }}>
+            <p className="main-heading">{data.name}</p>
+            <p style={{ margin: 0 }}>&nbsp;</p>
+            <p className="attribute-label">Spróbuj w temperaturze</p>
+            <p className="attribute-value">{attributes.get("pa_temperatura-spozycia")}</p>
+            <p style={{ margin: 0 }}>&nbsp;</p>
+            <p className="attribute-label">Szczep</p>
+            <p className="attribute-value">{attributes.get("pa_szczep")}</p>
+          </div>
+        </div>
+        <hr style={{ border: "none", height: "1px", backgroundColor: "#aaaaaa", width: "90%" }} />
+        <p className="mini-attribute">Struktura - <span style={{ fontWeight: "bold" }}>{attributes.get("pa_struktura")}</span></p>
+        <p className="mini-attribute">Aromaty - <span style={{ fontWeight: "bold" }}>{attributes.get("pa_aromaty")}</span></p>
+        <p className="mini-attribute">Pasujo do:</p>
+        {attributes.get("pa_pairings")?.split("|").map((x, i) =>
+          <p className="mini-attribute">{i + 1}) {x}</p>
+        )}
       </div>
     </div>
     <h1>{data.name}</h1>
-  </div>
+  </div >
 }
 
 function App() {
